ea07ecad87d7aaf1e26726929cbf44de31757efe
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2025-10-25 22:27:44 +0530

    Update testimonial.js

--- data/testimonial.js
+++ data/testimonial.js
@@ -3,7 +3,7 @@ export const testimonial = [
     quote:
       "Avia AI helped me structure my career like never before. The AI-guided industry growth made easy to grasp and apply!",
     author: "Ananya Singh",
-    image: "https://randomuser.me/api/portraits/women/68.jpg",
+   
     role: "Data Analyst",
     company: "Insightful Labs",
   },
@@ -11,7 +11,7 @@ export const testimonial = [
     quote:
       "The career insights and interview prep tools were spot on. I landed a role that aligns perfectly with my skills and interests!",
     author: "Rohan Mehta",
-    image: "https://randomuser.me/api/portraits/men/68.jpg",
+    
     role: "Software Developer",
     company: "TechSolutions Pvt. Ltd.",
   },
@@ -19,7 +19,7 @@ export const testimonial = [
     quote:
       "I never thought AI could improve my professional growth this much. The personalized feedback and resume suggestions were game-changing!",
     author: "Sanya Kapoor",
-    image: "https://randomuser.me/api/portraits/women/69.jpg",
+    
     role: "UX Designer",
     company: "Creative Minds Co.",
   },
